3ba162cc74f3ce1424751abd661f1b3f13efb21f
Young Ban <bany@student.ethz.ch>
2014-10-01 16:32:02 +0200

    Try now
    It should be working now i hope.
    Task 1-3: Should work without any problems.
    Should be able to connect to the T&H device.
    The SampleGattAttributes obviously doesnt contain the device data we
    want.

--- workspace/VS_glukas_BleSensirion/gen/ch/ethz/inf/vs/a1/glukas/ble/R.java
+++ workspace/VS_glukas_BleSensirion/gen/ch/ethz/inf/vs/a1/glukas/ble/R.java
@@ -25,16 +25,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
-        public static final int menu_refresh=0x7f080001;
-        public static final int menu_scan=0x7f080002;
-        public static final int menu_stop=0x7f080003;
+        public static final int action_settings=0x7f080008;
+        public static final int connection_state=0x7f080001;
+        public static final int data_value=0x7f080002;
+        public static final int device_address=0x7f080000;
+        public static final int device_name=0x7f080004;
+        public static final int gatt_services_list=0x7f080003;
+        public static final int menu_connect=0x7f080006;
+        public static final int menu_disconnect=0x7f080007;
+        public static final int menu_refresh=0x7f080005;
+        public static final int menu_scan=0x7f080009;
+        public static final int menu_stop=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int gatt_services_characteristics=0x7f030001;
+        public static final int listitem_device=0x7f030002;
+        public static final int progress=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int gatt_services=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
