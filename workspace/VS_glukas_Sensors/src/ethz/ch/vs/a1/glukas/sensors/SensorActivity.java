fbecffa2b15e69c711798377ce705768d4d0e1a1
Lukas <glukas@Lukass-MacBook-Pro.local>
2014-10-06 19:32:44 +0200

    (/=) Prevent crash for unknown sensor types.
    -show some text (“unsupported sensor”) instead
    (!) Fixed indentation.

--- workspace/VS_glukas_Sensors/src/ethz/ch/vs/a1/glukas/sensors/SensorActivity.java
+++ workspace/VS_glukas_Sensors/src/ethz/ch/vs/a1/glukas/sensors/SensorActivity.java
@@ -21,7 +21,8 @@ public class SensorActivity extends Activity implements SensorEventListener {
 	float[] g = {0f,0f,0f};
 	float alpha = 0.8f;
 	private String[] names;
-	
+	private static final String UNSUPPORTED_SENSOR = "Unsupported Sensor";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,183 +51,190 @@ public class SensorActivity extends Activity implements SensorEventListener {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	private void createNames(){
 		switch (getIntent().getExtras().getInt(MainActivity.EXTRA_SENSOR_TYPE)){
-		
-			case Sensor.TYPE_ACCELEROMETER: {
-				names = new String[3];
-				names[0] = "x in m/s\u00B2";
-				names[1] = "y in m/s\u00B2";
-				names[2] = "z in m/s\u00B2";
-				break;
-			}
-			
-			case Sensor.TYPE_AMBIENT_TEMPERATURE :{
-				names = new String[1];
-				names[0] = "Temperature in Celsius";	
-				break;
-			} 
-			
-			case Sensor.TYPE_ROTATION_VECTOR:{
-				names = new String[3];
-				names[0] = "x";
-				names[1] = "y";
-				names[2] = "z";
-				break;
-			} 
 
-			case Sensor.TYPE_GRAVITY: {
-				names = new String[3];
-				names[0] = "x in m/s\u00B2";
-				names[1] = "y in m/s\u00B2";
-				names[2] = "z in m/s\u00B2";
-				break;
-			}
+		case Sensor.TYPE_ACCELEROMETER: {
+			names = new String[3];
+			names[0] = "x in m/s\u00B2";
+			names[1] = "y in m/s\u00B2";
+			names[2] = "z in m/s\u00B2";
+			break;
+		}
 
-			case Sensor.TYPE_GYROSCOPE: {
-				names = new String[3];
-				names[0] = "x in rad/s";
-				names[1] = "y in rad/s";
-				names[2] = "z in rad/s";
-				break;
-			}
+		case Sensor.TYPE_AMBIENT_TEMPERATURE :{
+			names = new String[1];
+			names[0] = "Temperature in Celsius";	
+			break;
+		} 
 
-			case Sensor.TYPE_GYROSCOPE_UNCALIBRATED: {
-				names = new String[6];
-				names[0] = "x_uncalib in rad/s";
-				names[1] = "y_uncalib in rad/s";
-				names[2] = "z_uncalib in rad/s";
-				names[3] = "x_drift in rad/s";
-				names[4] = "y_drift in rad/s";
-				names[5] = "z_drift in rad/s";
-				break;
-			}
+		case Sensor.TYPE_ROTATION_VECTOR:{
+			names = new String[3];
+			names[0] = "x";
+			names[1] = "y";
+			names[2] = "z";
+			break;
+		} 
 
-			case Sensor.TYPE_LIGHT:{ 
-				names = new String[1];
-				names[0] = "Illumination in lx";
-				break;
-			} 
+		case Sensor.TYPE_GRAVITY: {
+			names = new String[3];
+			names[0] = "x in m/s\u00B2";
+			names[1] = "y in m/s\u00B2";
+			names[2] = "z in m/s\u00B2";
+			break;
+		}
 
-			case Sensor.TYPE_ORIENTATION: {
-				names = new String[3];
-				names[0] = "x in degrees";
-				names[1] = "y in degrees";
-				names[2] = "z in degrees";
-				break;
-			}
+		case Sensor.TYPE_GYROSCOPE: {
+			names = new String[3];
+			names[0] = "x in rad/s";
+			names[1] = "y in rad/s";
+			names[2] = "z in rad/s";
+			break;
+		}
 
-			case Sensor.TYPE_PRESSURE: {
-				names = new String[1];
-				names[0] = "Pressure in hPa";
-				break;
-			}
+		case Sensor.TYPE_GYROSCOPE_UNCALIBRATED: {
+			names = new String[6];
+			names[0] = "x_uncalib in rad/s";
+			names[1] = "y_uncalib in rad/s";
+			names[2] = "z_uncalib in rad/s";
+			names[3] = "x_drift in rad/s";
+			names[4] = "y_drift in rad/s";
+			names[5] = "z_drift in rad/s";
+			break;
+		}
 
-			case Sensor.TYPE_MAGNETIC_FIELD: {
-				names = new String[3];
-				names[0] = "x in uT";
-				names[1] = "y in uT";
-				names[2] = "z in uT";
-				break;
-			}
+		case Sensor.TYPE_LIGHT:{ 
+			names = new String[1];
+			names[0] = "Illumination in lx";
+			break;
+		} 
 
-			case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED: {
-				names = new String[6];
-				names[0] = "x_uncalib in uT";
-				names[1] = "y_uncalib in uT";
-				names[2] = "z_uncalib in uT";
-				names[3] = "x_bias in uT";
-				names[4] = "y_bias in uT";
-				names[5] = "z_bias in uT";
-				break;
-			}
-			
-			case Sensor.TYPE_PROXIMITY: {
-				names = new String[1];
-				names[0] = "Distance in cm";
-				break;
-			}
-			
-			case Sensor.TYPE_RELATIVE_HUMIDITY: {
-				names = new String[1];
-				names[0] = "Humidity in %";
-				break;
-			}
-			
-			case Sensor.TYPE_LINEAR_ACCELERATION: {
-				names = new String[3];
-				names[0] = "x in m/s\u00B2";
-				names[1] = "y in m/s\u00B2";
-				names[2] = "z in m/s\u00B2";
-				break;
-			}
+		case Sensor.TYPE_ORIENTATION: {
+			names = new String[3];
+			names[0] = "x in degrees";
+			names[1] = "y in degrees";
+			names[2] = "z in degrees";
+			break;
+		}
 
-			case Sensor.TYPE_STEP_COUNTER: {
-				names = new String[1];
-				names[0] = "Step count";
-				break;
-			}
-			
-			default: {
-				break;
-			}
+		case Sensor.TYPE_PRESSURE: {
+			names = new String[1];
+			names[0] = "Pressure in hPa";
+			break;
+		}
+
+		case Sensor.TYPE_MAGNETIC_FIELD: {
+			names = new String[3];
+			names[0] = "x in uT";
+			names[1] = "y in uT";
+			names[2] = "z in uT";
+			break;
+		}
+
+		case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED: {
+			names = new String[6];
+			names[0] = "x_uncalib in uT";
+			names[1] = "y_uncalib in uT";
+			names[2] = "z_uncalib in uT";
+			names[3] = "x_bias in uT";
+			names[4] = "y_bias in uT";
+			names[5] = "z_bias in uT";
+			break;
+		}
+
+		case Sensor.TYPE_PROXIMITY: {
+			names = new String[1];
+			names[0] = "Distance in cm";
+			break;
+		}
+
+		case Sensor.TYPE_RELATIVE_HUMIDITY: {
+			names = new String[1];
+			names[0] = "Humidity in %";
+			break;
+		}
+
+		case Sensor.TYPE_LINEAR_ACCELERATION: {
+			names = new String[3];
+			names[0] = "x in m/s\u00B2";
+			names[1] = "y in m/s\u00B2";
+			names[2] = "z in m/s\u00B2";
+			break;
+		}
+
+		case Sensor.TYPE_STEP_COUNTER: {
+			names = new String[1];
+			names[0] = "Step count";
+			break;
+		}
+
+		default: {
+			//nice fallback to prevent exceptions for unknown sensors
+			names = new String[1];
+			names[0] = UNSUPPORTED_SENSOR;
+			break;
+		}
 		}
 	}
-	
+
 	// One of the methods of SensorEventListener we have to implement
 	// If measured values change this method gets executed 
 	@Override
-	  public void onSensorChanged(SensorEvent event) {
+	public void onSensorChanged(SensorEvent event) {
 		ArrayList<HashMap<String,String>> sensorData = new ArrayList<HashMap<String,String>>();
 		HashMap<String,String> item;
 		float[] valuesCopied = Arrays.copyOf(event.values, event.values.length);
-		
-		
-			for (int k = 0; k < names.length; k++) {
-	
-				item = new HashMap<String,String>();
-				
-				if (k < names.length)	
-					item.put("item1",names[k]);
-				else
-					item.put("item1","Other value");
-				
+
+
+		for (int k = 0; k < names.length; k++) {
+
+			item = new HashMap<String,String>();
+
+			if (k < names.length)	
+				item.put("item1",names[k]);
+			else
+				item.put("item1","Other value");
+
+			if (!names[0].equals(UNSUPPORTED_SENSOR)) {
 				item.put("item2",String.valueOf(valuesCopied[k]));
-				sensorData.add(item);
+			} else {
+				item.put("item2", "");
+			}
+			sensorData.add(item);
 		}
 		//create adapter + fill list with it
 		simpleAdapter = new SimpleAdapter(this,sensorData, R.layout.list_two,new String[] {"item1","item2" }, new int[] {R.id.text1, R.id.text2});
-		
+
 		listView = (ListView) findViewById(R.id.listView1);
 		listView.setAdapter(simpleAdapter);
 
-	  }
-	
-	
-	
+	}
+
+
+
 	////
 	//Life cycle management
 	////
-	
-	@Override
-	  public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	  }
-	
-	@Override
-	  protected void onResume() {
-	    super.onResume();
-	    // register this class as a listener for events
-	    sensorManager.registerListener(this,
-	    	sensorManager.getDefaultSensor(getIntent().getExtras().getInt(MainActivity.EXTRA_SENSOR_TYPE)),
-	        SensorManager.SENSOR_DELAY_NORMAL);
-	  }
 
-	  @Override
-	  protected void onPause() {
-	    super.onPause();
-	    // Important to unregister when we leave the Activity
-	    sensorManager.unregisterListener(this);
-	  }
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// register this class as a listener for events
+		sensorManager.registerListener(this,
+				sensorManager.getDefaultSensor(getIntent().getExtras().getInt(MainActivity.EXTRA_SENSOR_TYPE)),
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Important to unregister when we leave the Activity
+		sensorManager.unregisterListener(this);
+	}
 
 }
