253d8170a69e316f51f6717dee8dbcad4f288e76
Young Ban <bany@student.ethz.ch>
2014-10-11 16:16:06 +0200

    Nothing important yet
    - Just empty classes
    - Permission added in case I need it

---
+++ Ex2/vs-glukas-client/src/ch/ethz/inf/vs/a2/sensor/ResponseParserImpl.java
@@ -0,0 +1,11 @@
+package ch.ethz.inf.vs.a2.sensor;
+
+public class ResponseParserImpl implements ResponseParser{
+
+	@Override
+	public double parseResponse(String response) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
