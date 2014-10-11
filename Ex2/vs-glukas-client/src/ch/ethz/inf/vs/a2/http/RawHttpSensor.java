e5927edece496df409969daa6704044ac39b38c1
Tom_Tenv <vdemotz@student.ethz.ch>
2014-10-11 13:28:28 +0200

    Added RawHttpSensor
    Add class, make it extend AbstractSensor

---
+++ Ex2/vs-glukas-client/src/ch/ethz/inf/vs/a2/http/RawHttpSensor.java
@@ -0,0 +1,19 @@
+package ch.ethz.inf.vs.a2.http;
+
+import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
+
+public class RawHttpSensor extends AbstractSensor {
+
+	@Override
+	public void getTemperature() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double parseResponse(String response) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
