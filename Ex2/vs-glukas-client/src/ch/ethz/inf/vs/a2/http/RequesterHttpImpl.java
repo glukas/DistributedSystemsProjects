3dc0572291242646aff0e43ca8eef3731f576722
Young Ban <bany@student.ethz.ch>
2014-10-14 23:41:13 +0200

    Solved Conflicts for RequestHttpImpl
    - I discarded Lukas changed in that class by accident and had to copy it
    into my own
    - hope everything runs fine

--- Ex2/vs-glukas-client/src/ch/ethz/inf/vs/a2/http/RequesterHttpImpl.java
+++ Ex2/vs-glukas-client/src/ch/ethz/inf/vs/a2/http/RequesterHttpImpl.java
@@ -7,63 +7,70 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import android.util.Log;
 
-public class RequesterHttpImpl implements Requester{
+public class RequesterHttpImpl implements Requester {
 
-	protected String request;
+	protected HttpUriRequest request;
 	protected String result = "";
 	int responseCode;
-	
-	
-	
+
+	/**
+	 * Subclasses can use this constructor if they want to set their own request
+	 * When calling executeRequest, this.request must be non null or there will
+	 * be a null pointer exception
+	 */
+	protected RequesterHttpImpl() {
+		this.request = null;
+	}
+
 	public RequesterHttpImpl(String request) {
-		this.request = request;
-		
-		}
+		this.request = new HttpGet(request);
+
+	}
 
 	@Override
-	public String executeRequest() throws NullPointerException{
-		
+	public String executeRequest() throws NullPointerException {
+
 		HttpClient client = new DefaultHttpClient();
-		HttpGet getrequest = new  HttpGet(this.request);
+
 		// Adding Headers
-		getrequest.setHeader("Connection", "close");
-		getrequest.addHeader("Accept", "text/html");
-		
-		
+		this.request.setHeader("Connection", "close");
+		this.request.addHeader("Accept", "text/html");
+
 		HttpResponse response;
-		
+
 		try {
-			response = client.execute(getrequest);
+			response = client.execute(this.request);
 			responseCode = response.getStatusLine().getStatusCode();
-			if(responseCode == 200)
-			{
-		        HttpEntity entity = response.getEntity();
-		        if(entity != null)
-		        
-		        {
-		         result = EntityUtils.toString(entity);             
-		               }
-		        response.getEntity().consumeContent();
+			if (responseCode == 200) {
+				HttpEntity entity = response.getEntity();
+				if (entity != null)
+				{
+					result = EntityUtils.toString(entity);
+				}
+			} else {
+				Log.e(this.toString(),
+						String.format("Http code %d", responseCode));
 			}
-			
+			// release resources
+			if (response.getEntity() != null) {
+				response.getEntity().consumeContent();
+			}
+
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
-		// TODO Auto-generated method stub
+
 		return result;
 	}
-	
-	
-	
-	
-	
-	
+
 }
