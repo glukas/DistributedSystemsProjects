2f5053dbc18497afba4aaae7c2d982008428d549
Lukas Gianinazzi <orionweissnix@gmail.com>
2014-10-16 19:50:30 +0200

    (/=) Proper XML parsing
    this is a bit overkill but it was explicitly stated we should use a
    proper parser.

--- Ex2/vs-glukas-client/src/ch/ethz/inf/vs/a2/sensor/ResponseParserXmlImpl.java
+++ Ex2/vs-glukas-client/src/ch/ethz/inf/vs/a2/sensor/ResponseParserXmlImpl.java
@@ -1,20 +1,55 @@
 package ch.ethz.inf.vs.a2.sensor;
 
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import android.util.Log;
 
 public class ResponseParserXmlImpl implements ResponseParser{
-
-	//TODO more proper xml parsing
+	
+	static final String TAG_NAME = "temperature";
+	
 	@Override
 	public double parseResponse(String response) {
-		String keyString = "<temperature>";
-		int index = response.indexOf(keyString);
-		if (index == -1) {
-			return Double.NaN;
-		}
-		Double parsed = Double.parseDouble(response.substring(index+keyString.length(), index+keyString.length()+4));
-		if (parsed == null) return Double.NaN;
-		return parsed;
+		
+        Double result = Double.NaN;
+        try {
+        	XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        	XmlPullParser xpp = factory.newPullParser();
+        	
+        	xpp.setInput( new StringReader (response) );
+        	int eventType = xpp.getEventType();
+        	boolean temperatureTag = false;
+        	while (eventType != XmlPullParser.END_DOCUMENT) {
+        		
+        		if (eventType == XmlPullParser.START_TAG) {
+        			
+        			if (xpp.getName().equals(TAG_NAME)) {
+        				temperatureTag = true;
+        			}
+
+        		} if (eventType == XmlPullParser.TEXT) {
+        			//Log.v("XML", xpp.getText());
+        			if (temperatureTag) {
+        				result = Double.valueOf(xpp.getText());
+        				if (result == null) {
+        					result = Double.NaN;
+        				}
+        				break;
+        			}
+        		}
+        		eventType = xpp.next();
+        	}
+        } catch (XmlPullParserException e) {
+        	e.printStackTrace();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        return result;
 	}
 
 }
