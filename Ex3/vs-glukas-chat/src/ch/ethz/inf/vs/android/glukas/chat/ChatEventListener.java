559ce3a96bdcb0fe01aaf26dfdac9fc62a8195ab
Lukas Gianinazzi <orionweissnix@gmail.com>
2014-10-26 22:31:14 +0100

    (+) Draft for ChatEventListener interface

--- Ex3/vs-glukas-chat/src/ch/ethz/inf/vs/android/glukas/chat/ChatEventListener.java
+++ Ex3/vs-glukas-chat/src/ch/ethz/inf/vs/android/glukas/chat/ChatEventListener.java
@@ -1,6 +1,9 @@
 package ch.ethz.inf.vs.android.glukas.chat;
 
 import java.util.EventListener;
+import java.util.Map;
+
+import ch.ethz.inf.vs.android.glukas.chat.ChatEventSource.ChatEvent;
 
 import android.os.Handler;
 
@@ -22,5 +25,43 @@ public interface ChatEventListener extends EventListener {
 	 */
 	public Handler getCallbackHandler();
 	
-	// TODO Add all necessary event triggers
+	public enum FailureReason {
+		notRegistered,
+		alreadyRegistered,
+		usernameAlreadyInUse,
+		nethzNotRecognized,
+		invalidLength,
+		notOnEthSubnet,
+		timeout,
+		unkownCommand;
+	};
+	
+	/*
+	
+	void onRegistrationSucceeded(int ownId, Lamport lamportClock, VectorClock, vectorClock);//TODO maybe separate onClientMapReceived
+	
+	void onRegistrationFailed(FailureReson reason);
+	
+	
+	void onGetClientMapping(Map<Integer, String> clientIdToUsernameMap);
+	
+	void onGetClientMappingFailed(FailureReason reason);
+	
+	
+	void onMessageDeliverySucceeded();//maybe provide handle to identify which message was delivered
+	
+	void onMessageDeliveryFailed(FailureReson reason);
+	
+	
+	void onMessageReceived(ChatEvent message);
+
+	
+	void onDeregistrarionSucceeded();
+	
+	void onDeregistrationFailed();
+
+
+	void onClientDeregistered(Integer clientId, String clientUsername);
+	
+	void onClientRegistered(Integer clientId, String clientUsername);*/
 }
